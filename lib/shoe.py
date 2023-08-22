848325bba5440c7906c4398c6e2ee22860f28224
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-08-22 22:43:32 +0300

    completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    
+    def __init__(self,brand,size,condition="worn"):
+        self.brand=brand
+        self._size=size
+        self.condition=condition
+        
+
+    def get_size(self):
+        return self._size  
+
+    def set_size(self,size):
+        if type(size) == int:
+            self._size=size
+        else:
+            print("size must be an integer")
+
+    size=property(get_size,set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition="New"
+
+
+nike=Shoe("Nike","9")
+nike.size=17  
+nike.cobble()
+print(nike.condition)
+            
